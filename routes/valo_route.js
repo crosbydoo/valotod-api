af4d23d38155419a19b8e1ea61aa7ac7511137d7
Crosbydev <xxristuaji141@gmail.com>
2023-10-19 14:20:50 +0700

    fix: optimasi code di valoroute

--- routes/valo_route.js
+++ routes/valo_route.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const { verifyToken } = require('../middleware/auth_middleware');
 const axios = require('axios');
 
+const API_VALO_URL = process.env.URL_VALO;
+
 router.get('/:endpointName', verifyToken, async (req, res) => {
     const { endpointName } = req.params;
 
     try {
         // Buat permintaan ke endpoint eksternal dengan nama dinamis
-        const response = await axios.get(`https://valorant-api.com/v1/${endpointName}`);
+        const response = await axios.get(`${API_VALO_URL}/${endpointName}`);
         const data = response.data; 
         res.json(data);
     } catch (error) {
